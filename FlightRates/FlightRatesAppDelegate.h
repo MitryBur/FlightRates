fa5a42caba047170b73e212686a7d806bf0218cd
Dmitry Burmistrov <Mitry@MitrysMac.local>
2011-08-15 21:58:31 +0400

    Initial Commit

---
+++ FlightRates/FlightRatesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlightRatesAppDelegate.h
+//  FlightRates
+//
+//  Created by Dmitry Burmistrov on 8/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlightRatesAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
